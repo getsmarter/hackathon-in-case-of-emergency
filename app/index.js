91dc2b4f944c66af3ded1734b6ec6f215a147706
kurvinh <khendricks@2u.com>
2020-05-07 18:05:36 +0200

    Adding .env for db acccess, able to connect to mlabs db instance

--- app/index.js
+++ app/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
-//require('dotenv').config();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
+require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+var MongoClient = require('mongodb').MongoClient;
+
+var dburl = process.env.DB;
+
+MongoClient.connect(dburl ,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function(err, db) {
+  if (err) throw err;
+    console.log("Database connected!");
+  db.close();
+});
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,8 +28,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
 });
 
 app.listen(port, () => {
