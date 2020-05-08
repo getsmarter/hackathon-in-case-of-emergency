6b61e19f8f4ff1aa0e530a9d783a6e0df2b3631e
iologbese2u <iologbese@2u.com>
2020-05-08 17:57:09 +0200

    Story 3 and 6 => Create organizations => List users in organization => Make user admin of organization

--- app/routes/organization.routes.js
+++ app/routes/organization.routes.js
@@ -8,4 +8,13 @@ router.post('/organizations', organization.create);
 // List organizations
 router.get('/organizations', organization.findAll);
 
+// Retrieve a single organization
+router.get('/organization/:organizationId', organization.findOne);
+
+// Update an organization
+router.put('/organization/:organizationId', organization.update);
+
+// Delete an organization
+router.delete('/organization/:organizationId', organization.delete);
+
 module.exports = router;
